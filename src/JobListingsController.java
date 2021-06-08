39998aeedb6e7f4189fbf480ed8e62cb8e3c9582
IamRiichey <dye18dgu@uea.ac.uk>
2021-06-08 11:43:49 +0100

    Help controller + help JobListings + Controller

--- src/JobListingsController.java
+++ src/JobListingsController.java
@@ -1,17 +1,64 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class JobListingsController
 {
     @FXML
+    private TableRow jobId, fishType, amountKg, payPerKg, dateDue, managedBy;
+
+    @FXML
+    private Button viewDetailsButton, acceptJobButton;
+
+    @FXML
     private Button helpButton, infoButton, jobListingButton, viewJobsButton, profileButton;
+
+    ObservableList<Job> jobList = FXCollections.observableArrayList();
+
+    public void initialize(){
+        displayJobs();
+    }
+
+    public void displayJobs(){
+//        LinkedList<Job> jobsList = DatabaseController.selectAllJobs();
+//        for(Job j : jobsList){
+//            jobId.setText(String.valueOf(j.getId()));
+//            fishType.setText(j.getFishType());
+//            amountKg.setText(String.valueOf(j.getAmountKg()));
+//            payPerKg.setText(String.valueOf(j.getPayPerKg()));
+//            dateDue.setText(j.getDateDue().toString());
+//            managedBy.setText(j.getIntermediaryName());
+//        }
+    }
+
+    public void viewDetails() throws IOException{
+        MyFishingPal.currentJob = DatabaseController.selectJob(1);
+        Stage stage = null;
+        Parent nextScene = null;
+        stage = (Stage) viewDetailsButton.getScene().getWindow();
+        nextScene = FXMLLoader.load(getClass().getResource("JobDetails.fxml"));
+        assert nextScene != null;
+        Scene scene = new Scene(nextScene);
+        stage.setScene(scene);
+        stage.setTitle("MyFishingPal");
+        stage.show();
+    }
+
+    public void acceptJob(){
+
+    }
 
     public void changeScene(ActionEvent event) throws IOException {
         Stage stage = null;
