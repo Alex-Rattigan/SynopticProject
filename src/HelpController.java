e9ea38551f8140e807b49377743655a03dc4b75b
Sophie Musk <sophiemusk2807@gmail.com>
2021-06-14 14:16:09 +0100

    Added comments

--- src/HelpController.java
+++ src/HelpController.java
@@ -1,3 +1,15 @@
+/*******************************************************************************************************************
+ * File: HelpController.java
+ *
+ * Date: 8/6/2021
+ *
+ * Author: Sophie Musk
+ *
+ * Description: This is the controller that adds functionality to the Help.fxml page, this page provides
+ * pointers and information for the user if they get stuck using the app and need help.
+ *
+ ******************************************************************************************************************/
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,11 +22,15 @@ import java.io.IOException;
 
 public class HelpController {
 
+    // These are the variable definitions for the navigation buttons
     @FXML
     private Button helpButton, infoButton, jobListingButton, viewJobsButton, profileButton;
 
+    // This method takes the user to the jobs view page
     public void goToJobView() throws IOException{
+        // checks to see if the user is an intermediary or fisher
         if(MyFishingPal.currentUser instanceof Intermediary){
+            // take the user to the intermediary page if intermediary
             Stage stage = null;
             Parent nextScene = null;
             stage = (Stage) viewJobsButton.getScene().getWindow();
@@ -25,6 +41,7 @@ public class HelpController {
             stage.setTitle("MyFishingPal");
             stage.show();
         } else if(MyFishingPal.currentUser instanceof Fisher){
+            // take the user to the fisher page if fisher
             Stage stage = null;
             Parent nextScene = null;
             stage = (Stage) viewJobsButton.getScene().getWindow();
@@ -37,6 +54,7 @@ public class HelpController {
         }
     }
 
+    // takes the user to the jobs listing page
     public void goToJobListing() throws IOException{
         Stage stage = null;
         Parent nextScene = null;
@@ -49,6 +67,7 @@ public class HelpController {
         stage.show();
     }
 
+    // takes the user to their profile page
     public void goToProfile() throws IOException{
         Stage stage = null;
         Parent nextScene = null;
@@ -61,6 +80,7 @@ public class HelpController {
         stage.show();
     }
 
+    // takes the user to the fishing info page
     public void goToFishingInfo() throws IOException{
         Stage stage = null;
         Parent nextScene = null;
@@ -73,10 +93,13 @@ public class HelpController {
         stage.show();
     }
 
+    //this is the method that changes the scenes for the navigation bar, this is code from Richey Blant's Software
+    //Engineering Coursework
     public void changeScene(ActionEvent event) throws IOException {
         Stage stage = null;
         Parent nextScene = null;
 
+        //this checks which button has been pressed and goes to the appropriate page
         if(event.getSource() == helpButton) {
             stage = (Stage) helpButton.getScene().getWindow();
             nextScene = FXMLLoader.load(getClass().getResource("Help.fxml"));
@@ -86,13 +109,16 @@ public class HelpController {
         } else if(event.getSource() == jobListingButton){
             stage = (Stage) jobListingButton.getScene().getWindow();
             nextScene = FXMLLoader.load(getClass().getResource("JobListings.fxml"));
+            //this one has to check which type of user is using the system before it can go to the correct page
         } else if(event.getSource() == viewJobsButton){
             stage = (Stage) viewJobsButton.getScene().getWindow();
-
+            //if the user is a fisher
             if(MyFishingPal.currentUser instanceof Fisher)
             {
+                //go to the fisher view
                 nextScene = FXMLLoader.load(getClass().getResource("FisherView.fxml"));
             }
+            //if the user is an intermediary
             else
             {
                 nextScene = FXMLLoader.load(getClass().getResource("IntermediaryView.fxml"));
