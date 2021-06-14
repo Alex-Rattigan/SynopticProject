705acaa3290333123b3c6965cb8b06dd6cd58256
IamRiichey <dye18dgu@uea.ac.uk>
2021-06-14 13:00:49 +0100

    Added comments to all my pages

--- src/JobListingsController.java
+++ src/JobListingsController.java
@@ -1,3 +1,15 @@
+/*******************************************************************************************************************
+ * File: JobListingsController.java
+ *
+ * Date: 8/6/2021
+ *
+ * Author: Richey Blant
+ *
+ * Description: This is the controller that adds functionality to the JobListings.fxml page, this page shows the
+ * available jobs for the fishers and allows them to accept the jobs that they are wanting to take.
+ *
+ ******************************************************************************************************************/
+
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -17,9 +29,7 @@ import java.util.Optional;
 
 public class JobListingsController
 {
-    @FXML
-    private Button helpButton, infoButton, jobListingButton, viewJobsButton, profileButton;
-
+    //these are the variable definitions for the table and the columns
     @FXML
     private TableView<Job> jobListingTable;
     @FXML
@@ -34,19 +44,28 @@ public class JobListingsController
     private TableColumn<Job, Double> pay;
     @FXML
     private TableColumn<Job, String> managed;
-
+    //these are the variable definitions for the page buttons
     @FXML
     private Button viewDetailsButton, acceptJobButton;
+    //these are the variable definitions for the navigation buttons
+    @FXML
+    private Button helpButton, infoButton, jobListingButton, viewJobsButton, profileButton;
 
+    //this creates a list of the available jobs
     LinkedList<Job> availableJobs = new LinkedList<>();
 
+    //this observes what row is selected for the user to be able to accept a job
     ObservableList<Job> selectedRows;
 
+    //this creates the alert that gets used in this class
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
 
+    //this is the method that runs as soon as the page is loaded
     public void initialize(){
+        //runs the createJobList method
         createJobList();
 
+        //this sets the information to the table to be viewed
         jobId.setCellValueFactory(new PropertyValueFactory<>("id"));
         fishType.setCellValueFactory(new PropertyValueFactory<>("fishType"));
         amount.setCellValueFactory(new PropertyValueFactory<>("amountKg"));
@@ -54,13 +73,17 @@ public class JobListingsController
         pay.setCellValueFactory(new PropertyValueFactory<>("payPerKg"));
         managed.setCellValueFactory(new PropertyValueFactory<>("intermediaryName"));
 
+        //this adds all the available jobs to the job listing table to be viewed
         jobListingTable.getItems().addAll(availableJobs);
+        //this is to select the table row that is wanting to be chosen for either more information or for accepting a job
         TableView.TableViewSelectionModel<Job> availableJobSelectionModel = jobListingTable.getSelectionModel();
         availableJobSelectionModel.setSelectionMode(SelectionMode.SINGLE);
         selectedRows = availableJobSelectionModel.getSelectedItems();
 
+        //this is what listens to the selection
         selectedRows.addListener(new ListChangeListener<Job>() {
             @Override
+            //once someone has selected a job, the buttons will become available
             public void onChanged(Change<? extends Job> change) {
                 viewDetailsButton.setDisable(false);
                 if(MyFishingPal.currentUser instanceof Fisher){
@@ -71,16 +94,19 @@ public class JobListingsController
 
     }
 
+    //this is the create job list method, this gets all the jobs that don't have an active fisher
     public void createJobList(){
-
+        //puts these jobs into the available jobs list
         availableJobs = CSVController.selectJobsWithoutFisher();
-
     }
 
+    //this is the method that goes to the view details page, this just gives the user a better look at the job
+    //where it isn't in table view
     public void viewJobDetails() throws IOException{
+        //sets the job that it wants to view to the selected job
         JobListingsDetailsController.setJobDetails(selectedRows.get(0));
 
-        // Open Job Details page
+        //changes scenes to the job details page
         Stage stage = null;
         Parent nextScene = null;
         stage = (Stage) viewDetailsButton.getScene().getWindow();
@@ -92,12 +118,15 @@ public class JobListingsController
         stage.show();
     }
 
+    //this is the method that lets the user accept a job
     public void acceptJob(){
+        //checks to make sure the user is a fisher
         if(MyFishingPal.currentUser instanceof Fisher){
+            //this is to select the table row that is wanting to be chosen for either more information or for accepting a job
             TableView.TableViewSelectionModel<Job> availableJobSelectionModel = jobListingTable.getSelectionModel();
             availableJobSelectionModel.setSelectionMode(SelectionMode.SINGLE);
             selectedRows = availableJobSelectionModel.getSelectedItems();
-
+            //this sets the current job to the one that is selected
             Job currentJob = selectedRows.get(0);
 
             //Check if the selected job is still available
@@ -115,11 +144,13 @@ public class JobListingsController
 
             }
 
-            //If job is available, proceed as normal:
+            //If job is available, proceed as normal
             if (currentJobLive) {
 
+                //sets the currently logged in user as the fisher for the job that is selected
                 currentJob.setFisherName(((Fisher) MyFishingPal.currentUser).getUsername());
 
+                //this puts up a warning to ask if the user is sure about accepting the job
                 alert.setTitle("Accept Job");
                 alert.setHeaderText("Are you sure you want to accept this job?");
                 Optional<ButtonType> result = alert.showAndWait();
@@ -155,13 +186,16 @@ public class JobListingsController
                 }
                 else if(result.get() == ButtonType.OK)
                 {
+                    //update the fisher id for a job to the current user
                     CSVController.updateFisherId(currentJob.getId(), (((Fisher) MyFishingPal.currentUser).getID()));
 
+                    //this clears the table, fills the table again
                     jobListingTable.getItems().clear();
                     createJobList();
                     jobListingTable.getItems().addAll(availableJobs);
                     jobListingTable.refresh();
 
+                    //this sets the buttons back to being disabled, until you select a row of the table
                     viewDetailsButton.setDisable(true);
                     acceptJobButton.setDisable(true);
                 }
@@ -174,6 +208,7 @@ public class JobListingsController
             //If selected job is not available, show an error
             else {
 
+                //this displays an error to say that the job isn't available
                 alert.setTitle("Job Already Taken");
                 alert.setHeaderText("Someone else has already taken this job. Please try another.");
                 Optional<ButtonType> result = alert.showAndWait();
@@ -183,12 +218,13 @@ public class JobListingsController
                     // do nothing
 
                 } else if(result.get() == ButtonType.OK) {
-
+                    //this clears the table, fills the table again
                     jobListingTable.getItems().clear();
                     createJobList();
                     jobListingTable.getItems().addAll(availableJobs);
                     jobListingTable.refresh();
 
+                    //this sets the buttons back to being disabled, until you select a row of the table
                     viewDetailsButton.setDisable(true);
                     acceptJobButton.setDisable(true);
 
@@ -203,10 +239,13 @@ public class JobListingsController
         }
     }
 
+    //this is the method that changes the scenes for the navigation bar, this is code from Richey Blant's Software
+    //Engineering Coursework
     public void changeScene(ActionEvent event) throws IOException {
         Stage stage = null;
         Parent nextScene = null;
 
+        //this checks which button has been pressed and goes to the appropriate page
         if(event.getSource() == helpButton) {
             stage = (Stage) helpButton.getScene().getWindow();
             nextScene = FXMLLoader.load(getClass().getResource("Help.fxml"));
@@ -216,13 +255,16 @@ public class JobListingsController
         } else if(event.getSource() == jobListingButton){
             stage = (Stage) jobListingButton.getScene().getWindow();
             nextScene = FXMLLoader.load(getClass().getResource("JobListings.fxml"));
+            //this one has to check which type of user is using the system before it can go to the correct page
         } else if(event.getSource() == viewJobsButton){
             stage = (Stage) viewJobsButton.getScene().getWindow();
-
+            //if the user is a fisher
             if(MyFishingPal.currentUser instanceof Fisher)
             {
+                //go to the fisher view
                 nextScene = FXMLLoader.load(getClass().getResource("FisherView.fxml"));
             }
+            //if the user is an intermediary
             else
             {
                 nextScene = FXMLLoader.load(getClass().getResource("IntermediaryView.fxml"));
--- src/ProfileController.java
+++ src/ProfileController.java
@@ -1,3 +1,16 @@
+/*******************************************************************************************************************
+ * File: ProfileController.java
+ *
+ * Date: 7/6/2021
+ *
+ * Author: Richey Blant
+ *
+ * Description: This is the profile controller, this is the page that makes the functionality for the Profile.fxml
+ * on this page, the user can see their information and have the options to change password, change mobile number
+ * or log out if they are finished using the system.
+ *
+ ******************************************************************************************************************/
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,26 +24,37 @@ import java.io.IOException;
 
 public class ProfileController {
 
+    //textfield variable definitions
     @FXML
     private TextField userForename, userSurname, userNumber, userUsername;
+    ///page button variable definitions
     @FXML
     private Button changePasswordButton, changeNumberButton, logoutButton;
+    //navigation bar button variable definitions
     @FXML
     private Button helpButton, infoButton, jobListingButton, viewJobsButton, profileButton;
 
+    //this is what gets run when the page gets launched
     public void initialize(){
         displayInfo();
     }
 
+    //this is the method that gets the information that gets displayed on the profile
     public void displayInfo(){
+        //if the currentuser is an intermediary, run this code
         if(MyFishingPal.currentUser instanceof Intermediary) {
+            //this gets the intermediaries information from the database cache
             Intermediary i = CSVController.selectIntermediaryRecord(((Intermediary) MyFishingPal.currentUser).getID());
+            //this sets the textfields to the information from the intermediary
             userUsername.setText(i.getUsername());
             userForename.setText(i.getFname());
             userSurname.setText(i.getSname());
             userNumber.setText(i.getMobileNo());
+            //if the currentuser is a fisher, run this code
         } else if(MyFishingPal.currentUser instanceof Fisher){
+            //this gets the fisher information from the database cache
             Fisher f = CSVController.selectFisherRecord(((Fisher) MyFishingPal.currentUser).getID());
+            //this sets the textfields to the information from the fisher
             userUsername.setText(f.getUsername());
             userForename.setText(f.getFname());
             userSurname.setText(f.getSname());
@@ -38,56 +62,39 @@ public class ProfileController {
         }
     }
 
+    //this is the method that goes to the page to change the users password once the button is pressed
     public void changePassword() throws IOException{
-        if(MyFishingPal.currentUser instanceof Intermediary) {
-            Stage stage = null;
-            Parent nextScene = null;
-            stage = (Stage) changePasswordButton.getScene().getWindow();
-            nextScene = FXMLLoader.load(getClass().getResource("ChangePassword.fxml"));
-            assert nextScene != null;
-            Scene scene = new Scene(nextScene);
-            stage.setScene(scene);
-            stage.setTitle("MyFishingPal");
-            stage.show();
-        } else if(MyFishingPal.currentUser instanceof Fisher){
-            Stage stage = null;
-            Parent nextScene = null;
-            stage = (Stage) changePasswordButton.getScene().getWindow();
-            nextScene = FXMLLoader.load(getClass().getResource("ChangePassword.fxml"));
-            assert nextScene != null;
-            Scene scene = new Scene(nextScene);
-            stage.setScene(scene);
-            stage.setTitle("MyFishingPal");
-            stage.show();
-        }
+        //changes the scene to the change password page
+        Stage stage = null;
+        Parent nextScene = null;
+        stage = (Stage) changePasswordButton.getScene().getWindow();
+        nextScene = FXMLLoader.load(getClass().getResource("ChangePassword.fxml"));
+        assert nextScene != null;
+        Scene scene = new Scene(nextScene);
+        stage.setScene(scene);
+        stage.setTitle("MyFishingPal");
+        stage.show();
     }
 
+    //this is the method that goes to the page to change the users mobile number once the button is pressed
     public void changeNumber() throws IOException{
-        if(MyFishingPal.currentUser instanceof Intermediary) {
-            Stage stage = null;
-            Parent nextScene = null;
-            stage = (Stage) changeNumberButton.getScene().getWindow();
-            nextScene = FXMLLoader.load(getClass().getResource("ChangeMobNo.fxml"));
-            assert nextScene != null;
-            Scene scene = new Scene(nextScene);
-            stage.setScene(scene);
-            stage.setTitle("MyFishingPal");
-            stage.show();
-        } else if(MyFishingPal.currentUser instanceof Fisher){
-            Stage stage = null;
-            Parent nextScene = null;
-            stage = (Stage) changeNumberButton.getScene().getWindow();
-            nextScene = FXMLLoader.load(getClass().getResource("ChangeMobNo.fxml"));
-            assert nextScene != null;
-            Scene scene = new Scene(nextScene);
-            stage.setScene(scene);
-            stage.setTitle("MyFishingPal");
-            stage.show();
-        }
+        //changes the scene to the change mobile number page
+        Stage stage = null;
+        Parent nextScene = null;
+        stage = (Stage) changeNumberButton.getScene().getWindow();
+        nextScene = FXMLLoader.load(getClass().getResource("ChangeMobNo.fxml"));
+        assert nextScene != null;
+        Scene scene = new Scene(nextScene);
+        stage.setScene(scene);
+        stage.setTitle("MyFishingPal");
+        stage.show();
     }
 
+    //this is the method that goes logs the current user out when it is pressed
     public void logOut() throws IOException{
+        //this sets the current user to null
         MyFishingPal.currentUser = null;
+        //changes the scene to the login page
         Stage stage = null;
         Parent nextScene = null;
         stage = (Stage) logoutButton.getScene().getWindow();
@@ -99,10 +106,12 @@ public class ProfileController {
         stage.show();
     }
 
+    //this is the method that changes the scenes for the navigation bar
     public void changeScene(ActionEvent event) throws IOException{
         Stage stage = null;
         Parent nextScene = null;
 
+        //this checks which button has been pressed and goes to the appropriate page
         if(event.getSource() == helpButton) {
             stage = (Stage) helpButton.getScene().getWindow();
             nextScene = FXMLLoader.load(getClass().getResource("Help.fxml"));
@@ -112,15 +121,19 @@ public class ProfileController {
         } else if(event.getSource() == jobListingButton){
             stage = (Stage) jobListingButton.getScene().getWindow();
             nextScene = FXMLLoader.load(getClass().getResource("JobListings.fxml"));
+            //this one has to check which type of user is using the system before it can go to the correct page
         } else if(event.getSource() == viewJobsButton){
             stage = (Stage) viewJobsButton.getScene().getWindow();
-
+            //if the user is a fisher
             if(MyFishingPal.currentUser instanceof Fisher)
             {
+                //go to the fisher view
                 nextScene = FXMLLoader.load(getClass().getResource("FisherView.fxml"));
             }
+            //if the user is an intermediary
             else
             {
+                //go to the intermediary view
                 nextScene = FXMLLoader.load(getClass().getResource("IntermediaryView.fxml"));
             }
         } else if(event.getSource() == profileButton){
